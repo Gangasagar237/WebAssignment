cf4011beb22fb8eae09d2f40d5858018535bc529
GANGASAGAR GOND <86286917+Gangasagar237@users.noreply.github.com>
2022-10-27 18:28:47 +0530

    304-02 form is completed

---
+++ 4-Advance-JS/402-DOM-Manipulation/AdvanceJS-402-02/402-02.js
@@ -0,0 +1,11 @@
+function showing(){
+    let c =document.getElementById('image');
+    // c.style.display="none"
+    c.scr = 
+    "https://i0.wp.com/allthingstalent.org/wp-content/uploads/2022/07/geekster.jpg?fit=1000%2C535&ssl=1"
+
+}
+function hideimg(){
+    document.getElementById('image').style.display="block";
+
+}
